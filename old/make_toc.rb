5a0df45b918344393648a5d13ee848a800250296
Jim V <jfv@stowers.org>
2012-08-27 15:44:10 -0500

    move make toc

---
+++ old/make_toc.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+input_filename = ARGV[0]
+
+lines = File.open(input_filename,'r').read
+
+  puts "<div id=\"toc\"><ul>"
+lines.each_line do |line|
+  if line =~ /^##(.*)\[(.*)\]/ and line[2..2] != "#"
+    title = $1
+    id = $2
+    # puts "<li><a href=\\"index.html##{id}\\">#{title.strip}</a></li>"
+    puts title
+
+  end
+end
+  puts "</ul></div>"
